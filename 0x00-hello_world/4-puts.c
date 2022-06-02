d63e30f9bc40c3d37b91fcce6a8443f379e7ff43
Tsion-sol <stsion25@gmail.com>
2022-06-02 16:19:24 -0700

    print > s exactly "Programming is like building a multilingual puzzle, followed by a new l in > e

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
-/**
- * main - entry point
- * 
- * Return: 0 number
- */
-int main(void)
-{
-	  puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+ *  * main - entry point                                                                                                                   
+ *   *                                                                                                                                      
+ *    * Return: 0 number                                                                                                                     
+ *     *                                                                                                                                      
+ *      */                                                                                                                                     
+                                                                                                                                        
+int main(void)                                                                                                                          
+	                                                                                                                                        
+{                                                                                                                                       
+	                                                                                                                                        
+	                puts("\"Programming is like building a multilingual puzzle");                                                           
+			                                                                                                                                        
+			                        return (0);                                                                                                     
+						                                                                                                                                        
 }
