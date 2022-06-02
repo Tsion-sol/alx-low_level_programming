6ab8db8833e714c2b849846dd02f493e431371c7
Tsion-sol <stsion25@gmail.com>
2022-06-02 16:27:47 -0700

    print > > s exactly "Programming is like building a multilingual puzzle, followed by a new  l > in
    > > e

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- *  * main - entry point                                                                                                                   
- *   *                                                                                                                                      
- *    * Return: 0 number                                                                                                                     
- *     *                                                                                                                                      
- *      */                                                                                                                                     
-                                                                                                                                        
-int main(void)                                                                                                                          
-	                                                                                                                                        
-{                                                                                                                                       
-	                                                                                                                                        
-	                puts("\"Programming is like building a multilingual puzzle");                                                           
-			                                                                                                                                        
-			                        return (0);                                                                                                     
-						                                                                                                                                        
+#include <stdio.h>
+/**
+ * main-entry poini
+ *
+ * Return:0 number
+ *
+ */
+int main(void)
+{
+ printf("with proper grammer, but the outcome is a piece of art,\n");
+ return(0);
 }
