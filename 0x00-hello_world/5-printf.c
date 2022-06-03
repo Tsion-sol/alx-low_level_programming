dedc11374d5e3816e1cbacbeaeb4331241af2e72
Tsion-sol <stsion25@gmail.com>
2022-06-03 03:00:31 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
 * main-Entrypoint    
- * Return: 0 number
- */
+* Return: 0 number
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
