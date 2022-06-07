f3813ec0a9ffbfdbab2473b7af533b55a10ceafe
Tsion-sol <stsion25@gmail.com>
2022-06-07 04:05:22 -0700

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "bestschool.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	-putchar('B');
+	-putchar('E');
+	-putchar('S');
+	-putchar('T');
+	-putchar('S');
+	-putchar('C');
+	-putchar('H');
+	-putchar('O');
+	-putchar('O');
+	-putchar('L');
+	-putchar('\n');
+
+	return (0);
+}
