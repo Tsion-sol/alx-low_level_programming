ee798ce8cd868c12e9335b2e409ab722dff0fd73
Tsion-sol <stsion25@gmail.com>
2022-06-08 12:19:52 -0700

    a function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "bestschool.h"
+#include "main.c"
 
 /**
  * main - Entry point
@@ -7,17 +7,15 @@
  */
 int main(void)
 {
-	-putchar('B');
-	-putchar('E');
-	-putchar('S');
-	-putchar('T');
-	-putchar('S');
-	-putchar('C');
-	-putchar('H');
-	-putchar('O');
-	-putchar('O');
-	-putchar('L');
-	-putchar('\n');
+	_putchar('_');
+	_putchar('P');
+	_putchar('U');
+	_putchar('T');
+	_putchar('C');
+	_putchar('H');
+	_putchar('A');
+	_putchar('R');
+	_putchar('\n');
 
 	return (0);
 }
