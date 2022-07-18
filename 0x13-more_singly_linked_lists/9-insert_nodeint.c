b4af4343c0ae03f99628df71b67bf1198051eec7
Tsion-sol <stsion25@gmail.com>
2022-07-18 14:49:29 -0700

    function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,37 +9,36 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	
-		listint_t *new_node, *temp;
-		unsigned int count;
+	listint_t *new_node, *temp;
+	unsigned int count;
 
-		temp = *head;
+	temp = *head;
 
-		count = 0;
-		while (temp && count < idx - 1)
+	count = 0;
+	while (temp && count < idx - 1)
+	{
+		temp = temp->next;
+		count++;
+	}
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		if (idx == 0)
 		{
-			temp = temp->next;
-			count++;
+			new_node->next = *head;
+			*head = new_node;
+			return (new_node);
 		}
-
-		new_node = malloc(sizeof(listint_t));
-		if (new_node != NULL)
+		if (count + 1 == idx)
 		{
-			new_node->n = n;
-			if (idx == 0)
-			{
-				new_node->next = *head;
-				*head = new_node;
-				return (new_node);
-			}
-			if (count + 1 == idx)
-			{
-				new_node->next = temp->next;
-				temp->next = new_node;
-				return (new_node);
-			}
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
 		}
-		free(new_node);
-		return (NULL);
+	}
+	free(new_node);
+	return (NULL);
 }
 		    
